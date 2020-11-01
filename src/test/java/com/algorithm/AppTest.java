9eaffa664e6e9882cfdaf990c5235e14ec2f2b50
illichso <illichso@gmail.com>
2020-11-01 14:30:33 +0100

    add moven project

---
+++ src/test/java/com/algorithm/AppTest.java
@@ -0,0 +1,15 @@
+package com.algorithm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
